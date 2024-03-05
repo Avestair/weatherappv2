b5965352bc95a7d2c9df414ad0a2794a4be82715
Avesta <92775375+Avestair@users.noreply.github.com>
2024-03-05 08:00:04 +0100

    Add files via upload

---
+++ components/Hero.jsx
@@ -0,0 +1,144 @@
+import Navbar from "./Navbar";
+import { useState } from "react";
+import axios from "axios";
+export default function Hero() {
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState("");
+  const [ishidden, setishidden] = useState(true);
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&&APPID=e0074362ee2bb2286abb4011edc02ab1`;
+
+  const searchLocation = (event) => {
+    if (event.key === "Enter") {
+      axios.get(url).then((response) => {
+        setData(response.data);
+        console.log(response.data);
+      });
+      setLocation("");
+      setishidden(false);
+    }
+  };
+
+  return (
+    <>
+      <Navbar />
+      <div className="grid gap-6 justify-items-center pt-24">
+        <input
+          type="text"
+          placeholder="enter a Location..."
+          className="bg-[rgba(100,116,139,0.7)] w-[230px]  text-white outline-none rounded-3xl p-3 md:w-[490px]"
+          onKeyDown={searchLocation}
+          value={location}
+          onChange={(event) => setLocation(event.target.value)}
+        />
+
+        <div
+          className={`grid ${
+            ishidden ? "hidden" : ""
+          }  md:gap-16 gap-8 rounded-3xl md:pb-8 font-inter text-white bg-[rgba(100,116,139,0.7)]`}
+        >
+          <div className="grid md:gap-16 gap-8 p-4 ml-2 md:ml-8">
+            <div className="flex gap-2 ">
+              <svg
+                className="md:w-4 md:h-5 w-3 h-4 mt-1 fill-white"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 384 512"
+              >
+                <path d="M215.7 499.2C267 435 384 279.4 384 192C384 86 298 0 192 0S0 86 0 192c0 87.4 117 243 168.3 307.2c12.3 15.3 35.1 15.3 47.4 0zM192 128a64 64 0 1 1 0 128 64 64 0 1 1 0-128z" />
+              </svg>
+              <p className="md:text-xl text-base">{data.name}</p>
+            </div>
+            <div className="flex gap-8 md:gap-24">
+              <svg
+                className={`md:w-16 md:h-16 w-8 h-8 fill-white ${
+                  data.weather?.[0]?.main?.toLowerCase() === "snow"
+                    ? ""
+                    : "hidden"
+                }`}
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 448 512"
+              >
+                <path d="M224 0c17.7 0 32 14.3 32 32V62.1l15-15c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-49 49v70.3l61.4-35.8 17.7-66.1c3.4-12.8 16.6-20.4 29.4-17s20.4 16.6 17 29.4l-5.2 19.3 23.6-13.8c15.3-8.9 34.9-3.7 43.8 11.5s3.8 34.9-11.5 43.8l-25.3 14.8 21.7 5.8c12.8 3.4 20.4 16.6 17 29.4s-16.6 20.4-29.4 17l-67.7-18.1L287.5 256l60.9 35.5 67.7-18.1c12.8-3.4 26 4.2 29.4 17s-4.2 26-17 29.4l-21.7 5.8 25.3 14.8c15.3 8.9 20.4 28.5 11.5 43.8s-28.5 20.4-43.8 11.5l-23.6-13.8 5.2 19.3c3.4 12.8-4.2 26-17 29.4s-26-4.2-29.4-17l-17.7-66.1L256 311.7v70.3l49 49c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0l-15-15V480c0 17.7-14.3 32-32 32s-32-14.3-32-32V449.9l-15 15c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l49-49V311.7l-61.4 35.8-17.7 66.1c-3.4 12.8-16.6 20.4-29.4 17s-20.4-16.6-17-29.4l5.2-19.3L48.1 395.6c-15.3 8.9-34.9 3.7-43.8-11.5s-3.7-34.9 11.5-43.8l25.3-14.8-21.7-5.8c-12.8-3.4-20.4-16.6-17-29.4s16.6-20.4 29.4-17l67.7 18.1L160.5 256 99.6 220.5 31.9 238.6c-12.8 3.4-26-4.2-29.4-17s4.2-26 17-29.4l21.7-5.8L15.9 171.6C.6 162.7-4.5 143.1 4.4 127.9s28.5-20.4 43.8-11.5l23.6 13.8-5.2-19.3c-3.4-12.8 4.2-26 17-29.4s26 4.2 29.4 17l17.7 66.1L192 200.3V129.9L143 81c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l15 15V32c0-17.7 14.3-32 32-32z" />
+              </svg>
+              <svg
+                className={`md:w-16 md:h-16 w-8 h-8 fill-gray-300 ${
+                  data.weather?.[0]?.main?.toLowerCase() === "clouds"
+                    ? ""
+                    : "hidden"
+                }`}
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 640 512"
+              >
+                <path d="M0 336c0 79.5 64.5 144 144 144H512c70.7 0 128-57.3 128-128c0-61.9-44-113.6-102.4-125.4c4.1-10.7 6.4-22.4 6.4-34.6c0-53-43-96-96-96c-19.7 0-38.1 6-53.3 16.2C367 64.2 315.3 32 256 32C167.6 32 96 103.6 96 192c0 2.7 .1 5.4 .2 8.1C40.2 219.8 0 273.2 0 336z" />
+              </svg>
+              <svg
+                className={`md:w-16 md:h-16 w-8 h-8 fill-orange-500 ${
+                  data.weather?.[0]?.main?.toLowerCase() === "clear"
+                    ? ""
+                    : "hidden"
+                }`}
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 512 512"
+              >
+                <path d="M361.5 1.2c5 2.1 8.6 6.6 9.6 11.9L391 121l107.9 19.8c5.3 1 9.8 4.6 11.9 9.6s1.5 10.7-1.6 15.2L446.9 256l62.3 90.3c3.1 4.5 3.7 10.2 1.6 15.2s-6.6 8.6-11.9 9.6L391 391 371.1 498.9c-1 5.3-4.6 9.8-9.6 11.9s-10.7 1.5-15.2-1.6L256 446.9l-90.3 62.3c-4.5 3.1-10.2 3.7-15.2 1.6s-8.6-6.6-9.6-11.9L121 391 13.1 371.1c-5.3-1-9.8-4.6-11.9-9.6s-1.5-10.7 1.6-15.2L65.1 256 2.8 165.7c-3.1-4.5-3.7-10.2-1.6-15.2s6.6-8.6 11.9-9.6L121 121 140.9 13.1c1-5.3 4.6-9.8 9.6-11.9s10.7-1.5 15.2 1.6L256 65.1 346.3 2.8c4.5-3.1 10.2-3.7 15.2-1.6zM160 256a96 96 0 1 1 192 0 96 96 0 1 1 -192 0zm224 0a128 128 0 1 0 -256 0 128 128 0 1 0 256 0z" />
+              </svg>
+              <svg
+                className={`w-16 h-16 fill-blue-600  ${
+                  data.weather?.[0]?.main?.toLowerCase() === "rain"
+                    ? ""
+                    : "hidden"
+                }`}
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 512 512"
+              >
+                <path d="M96 320c-53 0-96-43-96-96c0-42.5 27.6-78.6 65.9-91.2C64.7 126.1 64 119.1 64 112C64 50.1 114.1 0 176 0c43.1 0 80.5 24.3 99.2 60c14.7-17.1 36.5-28 60.8-28c44.2 0 80 35.8 80 80c0 5.5-.6 10.8-1.6 16c.5 0 1.1 0 1.6 0c53 0 96 43 96 96s-43 96-96 96H96zm-6.8 52c1.3-2.5 3.9-4 6.8-4s5.4 1.5 6.8 4l35.1 64.6c4.1 7.5 6.2 15.8 6.2 24.3v3c0 26.5-21.5 48-48 48s-48-21.5-48-48v-3c0-8.5 2.1-16.9 6.2-24.3L89.2 372zm160 0c1.3-2.5 3.9-4 6.8-4s5.4 1.5 6.8 4l35.1 64.6c4.1 7.5 6.2 15.8 6.2 24.3v3c0 26.5-21.5 48-48 48s-48-21.5-48-48v-3c0-8.5 2.1-16.9 6.2-24.3L249.2 372zm124.9 64.6L409.2 372c1.3-2.5 3.9-4 6.8-4s5.4 1.5 6.8 4l35.1 64.6c4.1 7.5 6.2 15.8 6.2 24.3v3c0 26.5-21.5 48-48 48s-48-21.5-48-48v-3c0-8.5 2.1-16.9 6.2-24.3z" />
+              </svg>
+              <div className="grid justify-center pb-4 md:pb-auto  gap-4">
+                {data.main ? (
+                  <h1 className="md:text-5xl text-xl font-bold md:pt-3">
+                    {data.main.temp.toFixed()}°C
+                  </h1>
+                ) : null}
+                {data.weather ? (
+                  <p id="condition" className="font-bold text-xs md:text-base">
+                    {data.weather[0].main}
+                  </p>
+                ) : null}
+              </div>
+              <div className="grid gap-4">
+                <div className="flex gap-4">
+                  <svg
+                    className="md:w-5 md:h-5 w-4 h-4 fill-white"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 384 512"
+                  >
+                    <path d="M192 512C86 512 0 426 0 320C0 228.8 130.2 57.7 166.6 11.7C172.6 4.2 181.5 0 191.1 0h1.8c9.6 0 18.5 4.2 24.5 11.7C253.8 57.7 384 228.8 384 320c0 106-86 192-192 192zM96 336c0-8.8-7.2-16-16-16s-16 7.2-16 16c0 61.9 50.1 112 112 112c8.8 0 16-7.2 16-16s-7.2-16-16-16c-44.2 0-80-35.8-80-80z" />
+                  </svg>
+                  {data.main ? (
+                    <p className="text-xs md:text-base">
+                      {data.main.humidity}%
+                    </p>
+                  ) : null}
+                </div>
+                <div className="flex gap-4">
+                  <svg
+                    className="md:w-5 md:h-5 w-4 h-4 fill-white"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 512 512"
+                  >
+                    <path d="M288 32c0 17.7 14.3 32 32 32h32c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32 14.3-32 32s14.3 32 32 32H352c53 0 96-43 96-96s-43-96-96-96H320c-17.7 0-32 14.3-32 32zm64 352c0 17.7 14.3 32 32 32h32c53 0 96-43 96-96s-43-96-96-96H32c-17.7 0-32 14.3-32 32s14.3 32 32 32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H384c-17.7 0-32 14.3-32 32zM128 512h32c53 0 96-43 96-96s-43-96-96-96H32c-17.7 0-32 14.3-32 32s14.3 32 32 32H160c17.7 0 32 14.3 32 32s-14.3 32-32 32H128c-17.7 0-32 14.3-32 32s14.3 32 32 32z" />
+                  </svg>
+                  {data.wind ? (
+                    <p className="text-xs md:text-base ">
+                      {data.wind.speed.toFixed()}KM/H
+                    </p>
+                  ) : null}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
